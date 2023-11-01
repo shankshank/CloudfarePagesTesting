81c2cf82978b65a6dc177dd2b0647565b4acdd89
shashank raghuraj <shashankraghuraj12@gmail.com>
2023-11-01 11:22:46 -0400

    changes to convert csv to json

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Hono } from "hono";
+import { Hono } from 'hono';
 const app = new Hono();
 import * as fs from 'fs';
 import csvParser from 'csv-parser';
@@ -22,12 +22,35 @@ interface Organization {
     departments: Department[];
 }
 
+function csvToJson(csvData: string): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+      const jsonArray: any[] = [];
+  
+      const lines = csvData.split('\n');
+      const headers = lines[0].split(',').map(header => header.trim());
+  
+      for (let i = 1; i < lines.length; i++) {
+        const values = lines[i].split(',');
+        const entry: any = {};
+  
+        for (let j = 0; j < headers.length; j++) {
+          entry[headers[j]] = values[j].trim();
+        }
+  
+        jsonArray.push(entry);
+      } 
+      resolve(jsonArray);
+    });
+  }
+
+//function convertCsvToJson(csvFilePath: string): Promise<Organization> {
 function convertCsvToJson(csvFilePath: string): Promise<Organization> {
+        
     const departments: { [key: string]: Department } = {};
 
     return new Promise((resolve, reject) => {
         const readStream = fs.createReadStream(csvFilePath);
-
+        //return "Inside csvtojson function..next step - "+csvFilePath;
         readStream
             .pipe(csvParser())
             .on('data', (row) => {
@@ -73,22 +96,51 @@ function convertCsvToJson(csvFilePath: string): Promise<Organization> {
 app.get("/", (ctx) => {
     return ctx.text(
         //creating a table of contents
-        "This is the \n\n1) type in /organization_chart to see the organization chart\n2)type in /me to get information in a json file about me!"
+        "This is the \n\n1) type in /orgchart to see the organization chart\n2)type in /me to get information in a json file about me!"
     )
 });
 
-app.get("/organization-chart", async (ctx) => {
-    const csvFilePath = 'general_data.csv'; // Adjust the path as needed
+app.get("/orgchart", async (ctx) => {
 
-    try {
-        const organizationData = await convertCsvToJson(csvFilePath); // Await the function call
-        console.log(organizationData);
+    // Include the entire CSV data here
+    const csvData = `name,department,salary,office,isManager,skill1,skill2,skill3\nJohn,CDN,80,Lisbon,FALSE,Caching,C++,AI\nIbrahim Gould,Bots,262,Austin,FALSE,HTML,Performance,GoLang\nVioleta Cortes,Developer Platform,98,Austin,FALSE,Caching,C++,AI\nBanks Fitzpatrick,CDN,250,Singapore,FALSE,Typescript,Rust,GoLang\nAnnabella Velasquez,Accounting,172,San Francisco,FALSE,HTML,Performance,Postgres`; 
+    
+    //const jsonArray = csvToJson(csvData);
+    //const jsonString = JSON.stringify(jsonArray);
+    //console.log(jsonString); 
 
-    } catch (error) {
-        console.error('Error:', error);
-        return ctx.text('Error occurred while processing the organization chart data.');
+    //return ctx.text(jsonString);
+
+    csvToJson(csvData)
+      .then((jsonArray) => {
+        console.log(jsonArray);
+        const jsonString = JSON.stringify(jsonArray);
+        console.log(jsonString);     
+      })
+      .catch((error) => {
+        return ctx.text('ERROR organization chart data.'+error);        
+      });
+
+      return ctx.text(' ! !   S U C C E S S   ! ! ');
+
+      /* OLD CODE
+      const fs = require('fs');
+      const csvFilePath = 'a.csv'; // Adjust the path as needed
+  
+      try {      
+          
+          const organizationData = await convertCsvToJson(csvFilePath); // Await the function call
+          const s = await convertCsvToJson(csvFilePath);
+          console.log(organizationData);
+  
+      } catch (error) {
+          console.error('Error:', error);
+          return ctx.text('Error occurred while processing the organization chart data.'+error);
+  
+        */
+
     }
-});
+);
 
 app.get("/me", (ctx) => {
     return ctx.text(
